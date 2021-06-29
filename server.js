12b2cf00067eef88e2ebe9996113d45217007e57
Kolade <chrisjay967@gmail.com>
2021-06-29 21:44:38 +0200

    Another commit

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.use(cors());
 app.use(express.json());
 
 // test route
+app.get("/", (req, res) => {
+  res.send("Welcome to 9jaPoll...");
+});
+
 app.get("/test", (req, res) => {
   res.send("Welcome to 9jaPoll...");
 });
